8a817fdaea03d6959269d5617c1cf7251e29a58e
assi1999 <149426108+assi1999@users.noreply.github.com>
2023-11-01 17:21:10 +0100

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -1,53 +1,70 @@
-# Importing the relevant libraries
+# Import libraries
 import pandas as pd
 import numpy as np
-from datetime import datetime
+import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-# Collect data from the CSV file
-data = pd.read_csv('/TSLA-1.csv')
+# Load data
+data = pd.read_csv('TSLA.csv')
 
-# Preprocess Data
-data['Date'] = pd.to_datetime(data['Date'])  # Convert the 'Date' column to a datetime format
-data.set_index('Date', inplace=True)  # Set 'Date' as the index in the dataset
+# ordere data by 'Date'
+data['Date'] = pd.to_datetime(data['Date'])
+data.sort_values('Date', inplace=True)
+data.set_index('Date', inplace=True)
 
-# Split Data
-train_data, test_data = train_test_split(data, test_size=0.2)  # Split data for training and testing sets
+# Checking for missing values 
+if data.isnull().sum().sum() != 0:
+    data.dropna(inplace=True)
 
-# Features of the CSV file
-features = ['Open', 'Low', 'High', 'Volume']  # Define the features used for prediction, using 'Open', 'Low', 'High', 'Volume'
+# Features and target for the regression model we arw using
+features = ['Open']
+target = 'Close'
 
-# Train the model with Linear Regression
-model = LinearRegression()  # Create a Linear Regression model
-model.fit(train_data[features], train_data['Close'])  # Train the model on the training data, using features to predict 'Close'
+# Split data sequentially (80% train, 20% test)
+train_size = int(0.8 * len(data))
+train_data = data[:train_size]
+test_data = data[train_size:]
 
-# Make the predictions
-predictions = model.predict(test_data[features])  # The model makes predictions on the test data
+# Train a linear regression model
+model = LinearRegression()
+model.fit(train_data[features], train_data[target])
 
-# Evaluate the model to assess its performance
-mse = mean_squared_error(test_data['Close'], predictions)  # Calculate mean squared error
-rmse = np.sqrt(mse)  # Calculate the root mean squared error
+# Predict on test data
+predictions = model.predict(test_data[features])
 
-print(f"Root Mean Squared Error: {rmse}")  # Print the root mean squared error
+# Scatter plot of Open vs. Close prices with regression line
+plt.figure(figsize=(20, 10))
+plt.scatter(test_data['Open'], test_data['Close'], color='blue', label='Actual Price')
+plt.plot(test_data['Open'], predictions, color='red', label='Regression Line')
+plt.legend()
+plt.title('Tesla Inc Stock Price Prediction')
+plt.xlabel('Open Price')
+plt.ylabel('Close Price')
+plt.tight_layout()
+plt.show()
 
-# Calculate the percentage score based on RMSE
-percentage_score = 100 - (rmse / test_data['Close'].mean()) * 100
+#Evaluate model using RMSE
+mse = mean_squared_error(test_data[target], predictions)
+rmse = np.sqrt(mse)
+print(f"Root Mean Squared Error: {rmse}")
+
+# Calculate and display percentage score
+percentage_score = 100 - (rmse / test_data[target].mean()) * 100
 print(f"Prediction Percentage Score: {percentage_score:.2f}%")
 
 # Function to find the nearest available date
 def find_nearest_date(date, data):
-    idx = (data.index - date).argmin()
-    return data.index[idx]
+    return data.index[np.abs(data.index - date).argmin()]
 
 # Predict the stock price for a specific date
-get_date_from_user = input("Enter the desired date (Format: YYYY-MM-DD): ")
-specific_date = pd.to_datetime(get_date_from_user)  # Convert user input to the correct Timestamp
-nearest_date = find_nearest_date(specific_date, data)
-specific_data = data.loc[nearest_date, features].values.reshape(1, -1)
-specific_prediction = model.predict(specific_data)[0]
-print(f"Predicted Price for {get_date_from_user}: {specific_prediction}")
-
-
+try:
+    get_date_from_user = input("Enter the desired date (Format: YYYY-MM-DD): ")
+    specific_date = pd.to_datetime(get_date_from_user)
+    nearest_date = find_nearest_date(specific_date, data)
+    specific_data = data.loc[nearest_date, features].values.reshape(1, -1)
+    specific_prediction = model.predict(specific_data)[0]
+    print(f"Predicted Price for {specific_date.strftime('%Y-%m-%d')}: {specific_prediction:.2f}")
+except:
+    print("Invalid date format. Please enter the date in the format YYYY-MM-DD.")
